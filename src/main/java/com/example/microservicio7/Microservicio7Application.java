efd724b4f6aa053bee338832209b42b021c22a21
gcorona72 <oyono1996@gmail.com>
2024-12-01 17:42:17 +0100

    init1

--- src/main/java/com/example/microservicio7/Microservicio7Application.java
+++ src/main/java/com/example/microservicio7/Microservicio7Application.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Microservicio7Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Microservicio7Application.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(Microservicio7Application.class, args);
+    }
 
 }
